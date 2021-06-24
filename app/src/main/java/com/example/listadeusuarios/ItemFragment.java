1ca8fab3a69118020a26934e2388b02fad6c9d2e
Édini Zanlucas <edinimz@gmail.com>
2021-06-24 14:59:01 -0400

    População de dados

--- app/src/main/java/com/example/listadeusuarios/ItemFragment.java
+++ app/src/main/java/com/example/listadeusuarios/ItemFragment.java
@@ -22,6 +22,9 @@ public class ItemFragment extends Fragment {
     // TODO: Customize parameters
     private int mColumnCount = 1;
 
+    int index; //index do ArrayList
+    ArrayList<Usuario> usuarios = new ArrayList();
+
     // TODO: Customize parameter argument names
     private static final String ARG_COLUMN_COUNT = "column-count";
 
@@ -63,12 +66,21 @@ public class ItemFragment extends Fragment {
         if (view instanceof RecyclerView) {
             Context context = view.getContext();
             RecyclerView recyclerView = (RecyclerView) view;
-            Usuario user = new Usuario("Carlos","2938923","carlos@gmail.com","9343849849", 78);
-            Usuario user1 = new Usuario("Jessica","12345945","jessica@gmail.com","8743849849", 34);
-            Usuario user2 = new Usuario("Marcos", "993049482", "marcos@gmail.com","83463374", "")
-            ArrayList<Usuario> usuarios = new ArrayList();
-            usuarios.add(0,user);
-            usuarios.add(1,user1);
+            
+            adicionaUsuarios("Carlos","2938923","carlos@gmail.com","9343849849", 78);
+            adicionaUsuarios("Jessica","12345945","jessica@gmail.com","8743849849", 34);
+            adicionaUsuarios("Marcos", "993049482", "marcos@gmail.com","83463374", 23);
+            adicionaUsuarios("Joana","07387870293","joana@gmail.com","9343897493", 67);
+            adicionaUsuarios("Leticia","3049810834","leticia@gmail.com","873948939", 25);
+            adicionaUsuarios("Felipe", "0938401943", "felipe@gmail.com","99947434", 22);
+            adicionaUsuarios("Joao","3481938434","joao@gmail.com","93438499434", 45);
+            adicionaUsuarios("Pedro","0293810983","pedro@gmail.com","884749849", 34);
+            adicionaUsuarios("Astrid", "938743497", "astrid@gmail.com","8346334094", 23);
+            adicionaUsuarios("Harry","38920938","harry@gmail.com","93438345", 56);
+            adicionaUsuarios("Eragon","0494092493","eragon@gmail.com","874381234", 34);
+            adicionaUsuarios("Katniss", "347937238", "katniss@gmail.com","83464321", 23);
+
+
             if (mColumnCount <= 1) {
                 recyclerView.setLayoutManager(new LinearLayoutManager(context));
             } else {
@@ -77,5 +89,10 @@ public class ItemFragment extends Fragment {
             recyclerView.setAdapter(new MyItemRecyclerViewAdapter(usuarios));
         }
         return view;
+    }
+    public void adicionaUsuarios(String nome, String CPF, String email, String telefone, int idade){
+        Usuario user = new Usuario(nome, CPF, email, telefone, idade);
+        usuarios.add(index,user);
+        index++;
     }
 }
